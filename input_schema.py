31a848e61a481bf37bd4b8d93f08c9b7e3fc3eb2
Niraj Agarwal <agarwal.niraj@gmail.com>
2024-04-11 23:19:25 -0400

    Update example image URL in input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["https://i.imgur.com/I0lT4OB.jpeg"]
+        'example': ["https://www.godrejinterio.com/imagestore/B2C/56101543SD00165/56101543SD00165_A2_803x602.jpg"]
     }
 }
