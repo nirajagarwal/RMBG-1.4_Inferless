19e5f7b5f1a18a248171f235edd9d15185e3e77d
Niraj Agarwal <agarwal.niraj@gmail.com>
2024-04-12 16:10:43 -0400

    Refactor image processing logic in app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from transformers import pipeline
-
 from io import BytesIO
-import base64
-import os
-
+import base64, requests
+from rembg import remove
+ 
 class InferlessPythonModel:
     def initialize(self):
         self.pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
 
     def infer(self, inputs):
         image_url = inputs["image_url"]
-        pillow_mask = self.pipe(image_url, return_mask = True) # outputs a pillow mask
-        pillow_image = self.pipe(image_url) # applies mask on input and returns a pillow image
-        
-        buff = BytesIO()
-        pillow_image.save(buff, format="PNG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
-        return { "generated_image_base64" : img_str }
+        #pillow_image = self.pipe(image_url) # applies mask on input and returns a pillow image        
+        #buff = BytesIO()
+        #pillow_image.save(buff, format="PNG")
+        #image_base64 = base64.b64encode(buff.getvalue()).decode()
+        image_output = remove(requests.get(image_url).content)
+        image_base64 = base64.b64encode(image_output).decode() 
+        return { "generated_image_base64" : image_base64  }
 
     def finalize(self):
         self.pipe = None
+        
