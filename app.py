7e35134a08f04f35b8e8293eb03f34e4b9a87d5a
Niraj Agarwal <agarwal.niraj@gmail.com>
2024-04-12 19:08:12 -0400

    Refactor image processing logic in app.py

--- app.py
+++ app.py
@@ -9,14 +9,18 @@ class InferlessPythonModel:
 
     def infer(self, inputs):
         image_url = inputs["image_url"]
-        #pillow_image = self.pipe(image_url) # applies mask on input and returns a pillow image        
-        #buff = BytesIO()
-        #pillow_image.save(buff, format="PNG")
-        #image_base64 = base64.b64encode(buff.getvalue()).decode()
         image_output = remove(requests.get(image_url).content)
         image_base64 = base64.b64encode(image_output).decode() 
         return { "generated_image_base64" : image_base64  }
 
+    def infer_orig(self,inputs):
+        image_url = inputs["image_url"]
+        pillow_image = self.pipe(image_url) # applies mask on input and returns a pillow image        
+        buff = BytesIO()
+        pillow_image.save(buff, format="PNG")
+        image_base64 = base64.b64encode(buff.getvalue()).decode()
+        return { "generated_image_base64" : image_base64  }
+        
     def finalize(self):
         self.pipe = None
         
