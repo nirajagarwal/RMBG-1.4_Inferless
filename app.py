d7e36af72c9205ab888d587a11db08e9c58fe153
Niraj Agarwal <agarwal.niraj@gmail.com>
2024-04-12 21:09:46 -0400

    Refactor image processing logic in app.py and remove unnecessary self.pipe() call

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from rembg import remove
  
 class InferlessPythonModel:
     def initialize(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        pass
 
     def infer(self, inputs):
         image_url = inputs["image_url"]
         image_output = remove(requests.get(image_url).content)
         image_base64 = base64.b64encode(image_output).decode() 
-        return { "generated_image_base64" : image_base64  }
+        return {"generated_image_base64" : image_base64}
         
     def finalize(self):
-        self.pipe = None
+        pass
         
